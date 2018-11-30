da17f44278004ac4219b6536c4acf4349cbde4d3
Eduardo Henrique <eduardo-hnq@hotmail.com>
2018-11-30 15:45:53 -0200

    Revert "criação de pastas, mv de arquivos para seu diretorio"
    This reverts commit 16ea7d4f24c568ab3cb5900949a2a5e186d24b33.

--- create_vm/create_vm_vagrant.sh
+++
@@ -1,6 +0,0 @@
-vagrant box list
-vagrant box add ubuntu/xenial64
-vagrant init ubuntu/xenial64
-vagrant up --debug
-vagrant halt
-vagrant global-status --prune
--- install/install_virtualbox.sh
+++
@@ -1,9 +0,0 @@
-apt -y remove virtualbox-\* 2>> rm-vb_vms.log && apt -y purge virtualbox-\* 2>> pg-vb_vms.log
-rm ~/"VirtualBox VMs" -Rf 2>> rm-vb_vms.log && rm ~/.config/VirtualBox/ -Rf 2>> rm-vb_config.log 
-apt -qy update  2>> apt-update-error.log && apt -qy dist-upgrade  2>> apt-dist-upgrade.log && apt -qy autoremove 2>> apt-autoremove-error.log
-apt-get -y install gcc make linux-headers-$(uname -r) dkms
-wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | apt-key add -
-wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | apt-key add -
-sh -c 'echo "deb http://download.virtualbox.org/virtualbox/debian $(lsb_release -sc) contrib" >> /etc/apt/sources.list'
-apt update && apt list --upgradable
-apt-get -qy install virtualbox-5.2
