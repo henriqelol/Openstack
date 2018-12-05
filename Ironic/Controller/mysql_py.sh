6820d6daf3ff939a3c809d026ac64229fbdab02c
Eduardo Henrique <eduardo-hnq@hotmail.com>
2018-12-05 09:19:13 -0200

    Correção password mysql

--- Ironic/Controller/mysql_py.sh
+++ Ironic/Controller/mysql_py.sh
@@ -1,5 +1,5 @@
 echo "Install PY Server and MYSQL Server"
-echo Y|apt-get install python-openstackclient 2>> openstackclient-error.log mysql-server 2>> mysql-server.log
+apt install  -qy python-openstackclient 2>> openstackclient-error.log && apt install -qy mysql-server 2>> mysql-server.log
 
 echo "Config Mysql"
 sed -i 's/127.0.0.1/10.0.0.11/' /etc/mysql/mysql.conf.d/mysqld.cnf
@@ -7,7 +7,4 @@ sed -i 's/^#max_connections/max_connections/' /etc/mysql/mysql.conf.d/mysqld.cnf
 sed -i 's/= 100$/= 4096/' /etc/mysql/mysql.conf.d/mysqld.cnf
 service mysql restart
 
-mysqladmin -u root -p '' password 'senhaDaVMdoMato'
-#mysql -h localhost -u root -p
-#uninstall plugin validate_password;
-echo "finish"
+mysql -u root -psenhaDaVMdoMato
