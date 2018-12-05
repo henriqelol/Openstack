de53ebc964ae52c76a3ea774ace7fb3de2acdc62
Eduardo Henrique <eduardo-hnq@hotmail.com>
2018-12-05 11:49:06 -0200

    Upload Ironic/Controller/rabbitmq.sh

--- Ironic/Controller/rabbitmq.sh
+++ Ironic/Controller/rabbitmq.sh
@@ -1,8 +1,7 @@
-echo "Install rabbitmq"
 apt -qy install rabbitmq-server 2>> rabbitmq-error.log
+
 systemctl enable rabbitmq-server.service
 systemctl start rabbitmq-server.service
 rabbitmqctl add_user openstack senhaDaVMdoMato
 rabbitmqctl set_permissions openstack "." "." ".*"
 rabbitmqctl start_app
-echo "Finish rabbitmq"
