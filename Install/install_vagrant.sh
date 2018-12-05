9d1f692691090f06812f8ced34c88b995f403af8
Eduardo Henrique <eduardo-hnq@hotmail.com>
2018-12-05 11:49:05 -0200

    Upload Install/install_vagrant.sh

--- Install/install_vagrant.sh
+++ Install/install_vagrant.sh
@@ -1,5 +1,7 @@
 apt -y remove vagrant 2>> rm-vagrant.log && apt -y purge vagrant 2>> pg-vagrant.log
+
 rm -rf /opt/vagrant 2>> rm-vagrant_opt.log & rm -f /usr/bin/vagrant 2>> rm-vagrant_bin.log
+
 wget https://releases.hashicorp.com/vagrant/2.2.1/vagrant_2.2.1_x86_64.deb
 dpkg -i vagrant_2.2.1_x86_64.deb
 vagrant -v
