3104c3cdbe3955c2edbb0a96eb4d2fb37695d7c9
Eduardo Henrique <eduardo-hnq@hotmail.com>
2018-12-05 11:49:06 -0200

    Upload Ironic/Controller/keystone.sh

--- Ironic/Controller/keystone.sh
+++ Ironic/Controller/keystone.sh
@@ -1,14 +1,15 @@
-echo "Create BD keystone"
 mysql -u root -psenhaDaVMdoMato
 CREATE DATABASE keystone;
 GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'localhost' IDENTIFIED BY 'senhaDaVMdoMato';
 GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'%' IDENTIFIED BY 'senhaDaVMdoMato';
 exit;
-echo "install keystone apache2"
-apt -y install keystone 2>> keystone-error.log
-apt -y install apache2 2>> apache2-error.log
-apt -y install libapache2-mod-wsgi 2>> libapache2-mod-wsgi-error.log 
-#apt -qy install python-oauth2client 2>> oauth2client-error.log
+
+apt -qy install keystone 2>> keystone-error.log
+
+apt -qy install apache2 2>> apache2-error.log
+
+apt -qy install libapache2-mod-wsgi 2>> libapache2-mod-wsgi-error.log 
+
 sed -i 's/^#memcache_servers = localhost:11211/memcache_servers = controller:11211/' /etc/keystone/keystone.conf
 sed -i 's/connection\ \=\ sqlite\:\/\/\/\/var\/lib\/keystone\/keystone\.db/connection\ \=\ mysql\+pymysql\:\/\/keystone\:senhaDaVMdoMato\@controller\/keystone/g' /etc/keystone/keystone.conf
 sed -i '/^\[token\]/a provider = fernet' /etc/keystone/keystone.conf
@@ -24,36 +25,10 @@ keystone-manage bootstrap --bootstrap-password senhaDaVMdoMato \
 echo "ServerName controller" >> /etc/apache2/apache2.conf
 service apache2 restart
 
-echo "export OS_USERNAME=admin" >> /home/vagrant/admin-openrc
-echo "export OS_PASSWORD=senhaDaVMdoMato" >> /home/vagrant/admin-openrc
-echo "export OS_PROJECT_NAME=admin" >> /home/vagrant/admin-openrc
-echo "export OS_USER_DOMAIN_NAME=Default" >> /home/vagrant/admin-openrc
-echo "export OS_PROJECT_DOMAIN_NAME=Default" >> /home/vagrant/admin-openrc
-echo "export OS_AUTH_URL=http://localhost:5000/v3" >> /home/vagrant/admin-openrc
-echo "export OS_IDENTITY_API_VERSION=3" >> /home/vagrant/admin-openrc
-
-echo "export OS_PROJECT_DOMAIN_NAME=Default" >> /home/vagrant/demon-openrc
-echo "export OS_USER_DOMAIN_NAME=Default" >> /home/vagrant/demon-openrc
-echo "export OS_PROJECT_NAME=demo" >> /home/vagrant/demon-openrc
-echo "export OS_USERNAME=demo" >> /home/vagrant/demon-openrc
-echo "export OS_PASSWORD=senhaDaVMdoMato" >> /home/vagrant/demon-openrc
-echo "export OS_AUTH_URL=http://controller:5000/v3" >> /home/vagrant/demon-openrc
-echo "export OS_IDENTITY_API_VERSION=3" >> /home/vagrant/demon-openrc
-echo "export OS_IMAGE_API_VERSION=2" >> /home/vagrant/demon-openrc
-
-mv /home/vagrant/backup/admin-openrc /home/vagrant/
-mv /home/vagrant/backup/demon-openrc /home/vagrant/
-
-source /home/vagrant/admin-openrc
-source /home/vagrant/demon-openrc
-
 openstack token issue
-
-echo "Openstack"
 openstack domain create --description "An Example Domain" example
 openstack project create --domain default --description "Service Project" service
 openstack project create --domain default --description "Demo Project" demo
 openstack user create --domain default --password senhaDaVMdoMato demo
 openstack role create user
 openstack role add --project demo --user demo user
-echo "Finish"
