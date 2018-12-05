bcff6a37bb08c2f3a927e99a46563b7c7230eb16
Eduardo Henrique <eduardo-hnq@hotmail.com>
2018-12-05 11:49:06 -0200

    Upload Ironic/Controller/chrony.sh

--- Ironic/Controller/chrony.sh
+++ Ironic/Controller/chrony.sh
@@ -1,4 +1,4 @@
-apt -qy install chrony 2>> apt-chrony-error.log
+apt -qy install chrony 2>> chrony-error.log
 
 sh -c "echo 'server 3.br.pool.ntp.org iburst' >> /etc/chrony/chrony.conf"
 sh -c "echo 'server 0.south-america.pool.ntp.org iburst' >> /etc/chrony/chrony.conf"
