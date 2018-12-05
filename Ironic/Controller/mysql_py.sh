c80b648cd9c8959fa359d792a6e3ab191a35aa7a
Eduardo Henrique <eduardo-hnq@hotmail.com>
2018-12-05 11:49:06 -0200

    Upload Ironic/Controller/mysql_py.sh

--- Ironic/Controller/mysql_py.sh
+++ Ironic/Controller/mysql_py.sh
@@ -1,10 +1,9 @@
-echo "Install PY Server and MYSQL Server"
 apt install  -qy python-openstackclient 2>> openstackclient-error.log && apt install -qy mysql-server 2>> mysql-server.log
 
-echo "Config Mysql"
 sed -i 's/127.0.0.1/10.0.0.11/' /etc/mysql/mysql.conf.d/mysqld.cnf
 sed -i 's/^#max_connections/max_connections/' /etc/mysql/mysql.conf.d/mysqld.cnf
 sed -i 's/= 100$/= 4096/' /etc/mysql/mysql.conf.d/mysqld.cnf
 service mysql restart
 
 mysql -u root -psenhaDaVMdoMato
+exit;
