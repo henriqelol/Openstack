a678a9154f6f241235d3a660adc02ae19945d4f7
Eduardo Henrique <eduardo-hnq@hotmail.com>
2018-12-05 11:49:06 -0200

    Upload Ironic/Controller/etcd.sh

--- Ironic/Controller/etcd.sh
+++ Ironic/Controller/etcd.sh
@@ -1,4 +1,4 @@
-apt -qy install etcd 2>> apt-etcd-error.log
+apt -qy install etcd 2>> etcd-error.log
 
 sed -i 's/# ETCD_NAME/ETCD_NAME/' /etc/default/etcd
 sed -i 's/# ETCD_DATA_DIR/ETCD_DATA_DIR/' /etc/default/etcd
@@ -9,7 +9,6 @@ sed -i 's/# ETCD_INITIAL_ADVERTISE_PEER_URLS/ETCD_INITIAL_ADVERTISE_PEER_URLS/' 
 sed -i 's/# ETCD_ADVERTISE_CLIENT_URLS/ETCD_ADVERTISE_CLIENT_URLS/' /etc/default/etcd
 sed -i 's/# ETCD_LISTEN_PEER_URLS/ETCD_LISTEN_PEER_URLS/' /etc/default/etcd
 sed -i 's/# ETCD_LISTEN_CLIENT_URLS/ETCD_LISTEN_CLIENT_URLS/' /etc/default/etcd
-
 sed -i 's/ETCD_NAME="hostname"/ETCD_NAME="controller"/g' /etc/default/etcd
 sed -i 's/ETCD_DATA_DIR="\/var\/lib\/etcd\/default"/ETCD_DATA_DIR="\/var\/lib\/etcd"/' /etc/default/etcd
 sed -i 's/ETCD_INITIAL_CLUSTER_STATE="existing"/ETCD_INITIAL_CLUSTER_STATE="new"/g' /etc/default/etcd
@@ -20,9 +19,8 @@ sed -i 's/ETCD_INITIAL_ADVERTISE_PEER_URLS="http\:\/\/localhost\:2380,http\:\/\/
 sed -i 's/ETCD_ADVERTISE_CLIENT_URLS="http\:\/\/localhost\:2379,http\:\/\/localhost\:4001"/ETCD_ADVERTISE_CLIENT_URLS="http\:\/\/10.0.0.11\:2379"/g' /etc/default/etcd
 sed -i 's/ETCD_LISTEN_PEER_URLS="http\:\/\/localhost\:2380,http\:\/\/localhost\:7001"/ETCD_LISTEN_PEER_URLS="http\:\/\/0.0.0.0\:2380"/g' /etc/default/etcd
 sed -i 's/ETCD_LISTEN_CLIENT_URLS="http\:\/\/localhost\:2379,http\:\/\/localhost\:4001"/ETCD_LISTEN_CLIENT_URLS="http\:\/\/10.0.0.11\:2379"/g' /etc/default/etcd
-
 sed -i 's/on-abnormal/on-failure/' /lib/systemd/system/etcd.service
 sed -i 's/ExecStart=\/usr\/bin\/etcd \$DAEMON_ARGS/ExecStart=\/usr\/bin\/etcd \-\-config\-file \/etc\/default\/etcd/' /lib/systemd/system/etcd.service
 
-sudo systemctl enable etcd >> etcd.log 2>> etcd-error.log
+sudo systemctl enable etcd 2>> etcd-error.log
 sudo systemctl start etcd
