b4cec621c6d624af8da5530acf61c7a139029ddb
Eduardo Henrique <eduardo-hnq@hotmail.com>
2018-12-05 11:49:06 -0200

    Upload Ironic/Controller/memcached.sh

--- Ironic/Controller/memcached.sh
+++ Ironic/Controller/memcached.sh
@@ -1,4 +1,5 @@
-apt -qy install memcached python-memcache 2>> apt-memcached-error.log
+apt -qy install memcached python-memcache 2>> memcached-error.log
+
 sed -i 's/127.0.0.1/10.0.0.11/' /etc/memcached.conf
 service memcached restart
 
