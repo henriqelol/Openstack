80d1fbcce99ab7cf348637a7e8637580a40280da
Eduardo Henrique <eduardo-hnq@hotmail.com>
2018-12-05 11:49:05 -0200

    Upload Ironic/All/connection.sh

--- Ironic/All/connection.sh
+++ Ironic/All/connection.sh
@@ -8,3 +8,14 @@ ping -c 4 block
 
 echo "Chrony"
 chronyc sources
+
+#Adicione a máquina Compute para o banco de dados do cell
+#Execute os comandos abaixo no controller.
+openstack compute service list --service nova-compute
+su -s /bin/sh -c "nova-manage cell_v2 discover_hosts --verbose" nova
+sed -i '/^\[scheduler\]/a discover_hosts_in_cells_interval = 300' /etc/nova/nova.conf
+
+openstack compute service list
+openstack catalog list
+openstack image list
+nova-status upgrade check
