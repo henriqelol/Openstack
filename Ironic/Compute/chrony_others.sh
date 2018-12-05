a02281dc099363571ea6ce1c70dfe477bbf644df
Eduardo Henrique <eduardo-hnq@hotmail.com>
2018-12-05 11:49:05 -0200

    Upload Ironic/Compute/chrony_others.sh

--- Ironic/Compute/chrony_others.sh
+++ Ironic/Compute/chrony_others.sh
@@ -1,7 +1,4 @@
-echo "Network Time Protocol (NTP)"
-echo "Chrony"
-
-apt -qy install chrony 2>> apt-chrony-error.log
+apt -qy install chrony 2>> chrony-error.log
 
 sudo sed -i 's/^pool/#pool/' /etc/chrony/chrony.conf
 sh -c "echo 'server controller iburst' >> /etc/chrony/chrony.conf"
