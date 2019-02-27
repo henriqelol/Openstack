6ea4787c8fd6a1e1e7f7f251595438fe2e6aa36b
henriqelol <eduardo-hnq@hotmail.com>
2019-02-27 11:40:22 -0300

    insert att in arc install_vagrant

--- Install/install_vagrant.sh
+++ Install/install_vagrant.sh
@@ -1,8 +1,14 @@
-apt -y remove vagrant 2>> rm-vagrant.log && apt -y purge vagrant 2>> pg-vagrant.log
+echo "Atualizar o Sistema ? [S][N]"
+read att
+if [[ "$att" == "S" ]];then
+	apt-get update && apt-get dist-upgrade && apt-get autoremove
+else
+	apt -y remove vagrant 2>> rm-vagrant.log && apt -y purge vagrant 2>> pg-vagrant.log
+	rm -rf /opt/vagrant 2>> rm-vagrant_opt.log & rm -f /usr/bin/vagrant 2>> rm-vagrant_bin.log
 
-rm -rf /opt/vagrant 2>> rm-vagrant_opt.log & rm -f /usr/bin/vagrant 2>> rm-vagrant_bin.log
-
-wget https://releases.hashicorp.com/vagrant/2.2.1/vagrant_2.2.1_x86_64.deb
-dpkg -i vagrant_2.2.1_x86_64.deb
-vagrant -v
-which vagrant
+	wget https://releases.hashicorp.com/vagrant/2.2.1/vagrant_2.2.1_x86_64.deb
+	dpkg -i vagrant_2.2.1_x86_64.deb
+	vagrant -v
+	which vagrant
+fi
+echo "Vagrant Instalado com sucesso!!"
