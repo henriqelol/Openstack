5321b6a92f89f38796e5c143543f6b3af37cee2a
Eduardo Henrique <eduardo-hnq@hotmail.com>
2019-02-28 01:51:38 -0300

    Add Logs, Correção install vagrant/VBox

--- Install/install_vagrant.sh
+++ Install/install_vagrant.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 echo "Atualizar o Sistema ? [S][N]"
 read att
-[ "$att" == "S" ] && apt-get update && apt-get dist-upgrade && apt-get autoremove
+[ "$att" == "S" ] && apt -qy update  2>> ../logs/update-error.log && apt -qy dist-upgrade  2>> ../logs/dist-upgrade.log && apt -qy autoremove 2>> ../logs/autoremove-error.log
 
 vagrant -v
 if [[ $? -eq 0 ]];then
 	echo "Vagrant já se encontra instalado!!"
 else 
-	#apt -y remove vagrant 2>> rm-vagrant.log && apt -y purge vagrant 2>> pg-vagrant.log
-	#rm -rf /opt/vagrant 2>> rm-vagrant_opt.log & rm -f /usr/bin/vagrant 2>> rm-vagrant_bin.log
-	wget https://releases.hashicorp.com/vagrant/2.2.1/vagrant_2.2.1_x86_64.deb
-	dpkg -i vagrant_2.2.1_x86_64.deb
+	apt -y remove vagrant 2>> rm-vagrant.log && apt -y purge vagrant 2>> pg-vagrant.log
+	rm -rf /opt/vagrant 2>> rm-vagrant_opt.log & rm -f /usr/bin/vagrant 2>> rm-vagrant_bin.log
+	#wget https://releases.hashicorp.com/vagrant/2.2.1/vagrant_2.2.1_x86_64.deb
+	#dpkg -i vagrant_2.2.1_x86_64.deb
+	wget https://releases.hashicorp.com/vagrant/2.2.4/vagrant_2.2.4_x86_64.deb
+	dpkg -i vagrant_2.2.4_x86_64.deb
 	vagrant -v
 	which vagrant
+	echo "Vagrant Instalado com sucesso!!"
 fi
-
-echo "Vagrant Instalado com sucesso!!"
--- Install/install_virtualbox.sh
+++ Install/install_virtualbox.sh
@@ -1,19 +1,20 @@
+#!/bin/bash
 echo "Atualizar o Sistema ? [S][N]"
 read att
-if [[ "$att" == "S" ]];then
-	
-apt -y remove virtualbox-\* 2>> rm-vb_vms.log && apt -y purge virtualbox-\* 2>> pg-vb_vms.log
+[ "$att" == "S" ] && apt -qy update  2>> ../logs/update-error.log && apt -qy dist-upgrade  2>> ../logs/dist-upgrade.log && apt -qy autoremove 2>> ../logs/autoremove-error.log
 
-rm ~/"VirtualBox VMs" -Rf 2>> rm-vb_vms.log && rm ~/.config/VirtualBox/ -Rf 2>> rm-vb_config.log 
-
-apt -qy update  2>> update-error.log && apt -qy dist-upgrade  2>> dist-upgrade.log && apt -qy autoremove 2>> autoremove-error.log
-
-apt-get -y install gcc make linux-headers-$(uname -r) dkms
-
-wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | apt-key add -
-wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | apt-key add -
-sh -c 'echo "deb http://download.virtualbox.org/virtualbox/debian $(lsb_release -sc) contrib" >> /etc/apt/sources.list'
-
-apt update && apt list --upgradable
-
-apt-get -qy install virtualbox-5.2 2>> vb-erro.log
+virtualbox -v
+if [[ $? -eq 0 ]];then
+	echo "VirtualBox já se encontra instalado!!"
+else 
+	apt -y remove virtualbox-5.\* 2>> rm-vb_vms.log && apt -y purge virtualbox-5.\* 2>> pg-vb_vms.log
+	#rm ~/"VirtualBox VMs" -Rf 2>> rm-vb_vms.log && rm ~/.config/VirtualBox/ -Rf 2>> rm-vb_config.log 
+	apt-get -y install gcc make linux-headers-$(uname -r) dkms
+	wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | apt-key add -
+	wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | apt-key add -
+	sh -c 'echo "deb http://download.virtualbox.org/virtualbox/debian $(lsb_release -sc) contrib" >> /etc/apt/sources.list'
+	apt remove virtualbox virtualbox-5.*
+	apt update && apt list --upgradable
+	apt-get -qy install virtualbox-6.0 2>> vb-erro.log
+	echo "VirtualBox Instalado com sucesso!!"
+fi
