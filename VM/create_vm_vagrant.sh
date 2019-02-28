88212c32851fbcc573c87dc14ecb7126b64b5e26
Eduardo Henrique <eduardo-hnq@hotmail.com>
2019-02-28 02:16:08 -0300

    Correção Create vm vagrant

--- VM/create_vm_vagrant.sh
+++ VM/create_vm_vagrant.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
 vagrant box list
-vagrant box add ubuntu/xenial64
+vagrant box add ubuntu/xenial64 --force
 vagrant init ubuntu/xenial64
 vagrant up --debug
 vagrant halt
 vagrant global-status --prune
+echo "Máquina Vagrant Finalizada!!"
